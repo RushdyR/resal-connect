e7c9b3672d8557ee1ef5d7c2414e16eb7f87b828
ahmadstarving <akusumah@resal.me>
2026-04-06 15:24:57 +0300

    feat(security): allow single security

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
       {
         source: "/(.*)",
         headers: [
-          { key: "X-Frame-Options", value: "ALLOW-FROM https://stage-app.boonus.app/" },
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
           { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" },
@@ -22,8 +22,7 @@ const nextConfig = {
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https:",
               "connect-src 'self'",
-              "frame-ancestors 'self' https://stage-app.boonus.app/",
-              "frame-ancestors 'self' http://localhost:3000/",
+              "frame-ancestors 'self' https://merchant.resal.me https://stage-app.boonus.app http://localhost:3000",
             ].join("; "),
           },
         ],
